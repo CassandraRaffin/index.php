f85ea7ead0eab091c09eac3640e5d46218f8346f
unknown <cassandra.raffin@efrei.net>
2018-11-14 23:47:03 +0100

    first

--- index.php
+++ index.php
@@ -1,40 +1,78 @@
-
 <html>
-<body style="text-align: center; front-size: 50px;">
-<br>
+<head>
+
+<header>
+ <h2>Friend book</h2>
+</header>
+
+
+
+<style>
+
+/* Style the header */
+header {
+ background-color: #666;
+ padding: 30px;
+ text-align: center;
+ font-size: 35px;
+ color: white;
+}
+/* Style the footer */
+footer {
+ background-color: #777;
+ padding: 10px;
+ text-align: center;
+ color: white;
+}
+</style>
+</head>
+
+
+<body>
+
+<form action="index1.php" method="post">
+<br>Name: <input type="text" name="name">
+<input type="submit">
+</form>
+
 <?php
-if (!isset($_GET['op']) || !isset($_GET['x']) || !isset($_GET['y'])) {
-echo " <h1> Incorret or incomplete data </h1>";
-exit();
-}
+echo "<h1>My best friends: </h1>";
+    
+echo "<ul>";
 
-$x = $_GET['x'];
-$y = $_GET['y'];
+$filename = 'newfile.txt';
 
-switch ($_GET['op']) {
-case 'sum':
-$result = $x + $y;
-echo "<h1>$x + $y = $result</h1>";
-break;
-case 'subtract': 
-$result = $x - $y;
-echo "<h1>$x - $y = $result</h1>";
-break;
-case 'divide': 
-if ($y == 0) {
-echo "<h1>Cannot divide by zero!</h1>";
+$file = fopen( $filename, "r" );
+    
+if( $file != false ) {
+    
+    while(!feof($file)){
+        
+        $friend = fgets($file);
+        if(strlen($friend) > 0) {
+            echo "<li>$friend</li>";
+        }
+    }
 }
-else {
-$result = $x / $y;
-echo "<h1>$x / $y = $result</h1>";
-break;
+fclose( $file );
+    
+if (isset($_POST['name']) && strlen($_POST['name']) > 0) {
+    $name = $_POST['name'];
+    $file = fopen( $filename, "a" );
+    if($file != false){
+         echo "<b><li>$name</li></b>"; 
+         fwrite( $file, $name . "\n" );
+         fclose( $file );
+    }
+
 }
-case 'multiply': 
-$result = $x * $y;
-echo "<h1>$x * $y = $result</h1>";
-break;
-}
+echo "</ul>";
 
 ?>
-</body> 
+
+</body>
+
+<footer>
+ <p>Footer</p>
+</footer>
 </html>
